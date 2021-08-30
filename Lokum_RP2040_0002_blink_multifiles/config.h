88d062fab354cf8d4c16eda3c627a5288e95789e
walidamriou <walidamriou@gmail.com>
2021-08-30 15:48:19 +0200

    Create config.h

---
+++ Lokum_RP2040_0002_blink_multifiles/config.h
@@ -0,0 +1,21 @@
+/**
+ * @file config.h
+ * @brief header file of config of the code
+ * 
+ * @author Walid Amriou
+ * @date 08/30/2021
+ * 
+ * This project blink the led on Raspberry pi pico and print "Raspberry pi pico LED Blink" every Blink
+ * (The LED ON for 2s and OFF for 3s)
+ * 
+ */
+
+#ifndef CONFIG_H_INCLUDED
+#define CONFIG_H_INCLUDED
+
+#define raspberrypi_pico_led 25
+#define led_on_2s 2000
+#define led_off_3s 2000
+
+#endif
+
